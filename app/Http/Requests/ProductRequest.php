6040e8f03bd94a83afb3c9e7f4b06c2a47fb3119
viktor.pidlatiuk <victor.pidlatiuk@sigma.software>
2023-09-29 12:48:43 +0200

    initial commit

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize():bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules():array
+    {
+        return [
+            'title'=>'required|max:100|unique:products,title',
+            'pictures'=> 'required',
+            'pictures.*' => 'mimes:jpg,png|dimensions:min_width=1280',
+            'category_id' => 'required|exists:categories,id',
+            'description'=>'required|max:500',
+            'price' => 'required|numeric|between:0.1,9999999.99'
+        ];
+    }
+}
